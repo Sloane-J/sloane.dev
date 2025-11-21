c8ea981ff70f3c2c7296866e934e2bacc6db1310
Samuel Dorkey Junior <samueldorkeyjr@gmail.com>
2025-11-21 21:59:19 +0000

    ran build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://sloanedev.vercel.app',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   vite: {
     resolve: {
